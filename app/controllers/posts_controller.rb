e501267289f900aaca1a500b0d4490fb78e3af3d
Rochelle Katzman <zelrochelle@gmail.com>
2023-02-03 10:46:05 -0500

    commit tree dd520c6cd618b29ed48191c3526cca2f08300525

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,10 +8,13 @@ class PostsController < ApplicationController
 
   def update
     post = Post.find(params[:id])
-
-    post.update(post_params)
-
-    render json: post
+    byebug
+    if post.valid?
+      post.update(post_params)
+      render json: post
+    else
+      render json: { errors: post.errors }, status: :unprocessable_entity 
+    end 
   end
 
   private
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ApplicationRecord
+    belongs_to :author
+    validates :title, presence: true 
+    validates :category, inclusion: { in: ["Fiction", "Non-Fiction"]}
+    validates :content, length: { minimum: 100 }
 end
